58d1f9ddd6ac5e7f60d707757929d98d0ef68341
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-18 14:14:33 +0000

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get("/users", { :controller => "users", :action => "index" })
+  get("/", :controller => "users", :action => "index")
 
+  get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+  get("/update_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  post("/add_comment/:path_id", { :controller => "photos", :action => "comment" })
 end
