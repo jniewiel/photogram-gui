73b3d83ff51459127caed110a99a2fdfa506fa29
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-18 14:14:52 +0000

    added photos controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,89 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render({ :template => "photo_templates/index" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show" })
+  end
+
+  def destroy
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos", { :notice => "Photo deleted successfully." })
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    redirect_to("/photos/" + a_new_photo.id.to_s, { :notice => "Photo uploaded successfully." })
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+    
+    next_url = "/photos/" + the_photo.id.to_s
+    redirect_to(next_url)
+  end
+
+  def comment
+    the_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => the_id})
+    the_photo = matching_photos.at(0)
+
+    the_photo_comments = the_photo.comments
+
+    a_new_comment = Comment.new
+
+    input_author_id = params.fetch("query_author_id")
+    input_comment = params.fetch("query_comment")
+
+    a_new_comment.body = input_comment
+    a_new_comment.author_id = input_author_id
+
+    ??
+
+    a_new_comment.save
+    the_photo.save
+
+    redirect_to("/photos/" + the_photo.id.to_s, { :notice => "Comment uploaded successfully." })
+  end
+end
